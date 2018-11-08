3eae67b259ae33095a8c7fe2ba26c664c9bd4ad7
francxxx <f.rivae@gmail.com>
2018-11-08 09:49:04 +0100

    Francesco configuration

--- src/main/java/SoEproj/App.java
+++ src/main/java/SoEproj/App.java
@@ -10,7 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Ciao Wael, Vittorio! La classe che hai creato funziona!" );
+        System.out.println( "Ciao Wael, Vittorio, Francesco! La classe che hai creato funziona!" );
         imptest sd = new imptest();        
         sd.stamp();
 
